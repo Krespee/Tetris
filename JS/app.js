e5f8d0eaec86cd8cccfed88df1d92134d4d69583
Nayn <naynzahir@gmail.com>
2023-10-03 06:19:51 -0300

    Creando Contenedor y cajas

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+
+const createBox = () =>{
+    const fragment = document.createDocumentFragment()
+    for (let i = 0; i < 200; i++) {
+        let box = document.createElement("DIV");
+        box.classList.add("box");
+        fragment.appendChild(box)
+    }
+    container.appendChild(fragment)
+}
+
+createBox()

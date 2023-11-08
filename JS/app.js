a2bcc6b259b6b3a77a46cf6b6e747471f5be01f6
Nayn <naynzahir@gmail.com>
2023-11-08 06:06:23 -0300

    añdaiendo score

--- JS/app.js
+++ JS/app.js
@@ -5,13 +5,38 @@ let currentForm = ""
 let currentEjeLine = "horizontal"; 
 let currentEjeL = "horizontal"; 
 let currentEjeZ = "vertical"; 
-
+let currentEjeT = "vertical"; 
+const score = document.getElementById("score")
+let scorePoints = 0;
+const lines = document.getElementById("lines")
+let linesCount = 0;
+const level = document.getElementById("level")
+let levelCount = 0;
+const maxScore = document.getElementById("max-score")
+maxScore.textContent = Number(localStorage.getItem("score"))
+//activar musica
 
 // document.addEventListener("DOMContentLoaded", function() {
 //     var audio = document.getElementById("miAudio");
 //     audio.play();
 //   });
 //
+
+//guardar puntaje
+const updateScore = () =>{
+    localStorage.setItem('score', scorePoints);
+}
+//
+
+//añadiendo puntos
+
+const addPoints=(points)=>{
+    scorePoints += points;
+    score.textContent = scorePoints
+    updateScore()
+    if(points )
+}
+
 
 const container = document.querySelector(".container");
 
@@ -28,7 +53,12 @@ const createBox = () =>{
 createBox()
 
 
+//añadiendo numeros de lineas destruidas
 
+const addLines=(lin)=>{
+    linesCount += lin;
+    lines.textContent = linesCount
+}
 //colores
 const colors = ["block-blue","block-red","block-green","block-yellow","block-white"]
 const randomColor = colors =>{
@@ -82,6 +112,7 @@ const createBlock = (color, form) =>{
     currentEjeLine = "horizontal"; 
     currentEjeL = "horizontal"; 
     currentEjeZ = "vertical"; 
+    currentEjeT = "vertical"; 
     if (form == "line") {
         positionForm = [4,14,24,34,44]
         createBlock5(color)
@@ -95,7 +126,7 @@ const createBlock = (color, form) =>{
         positionForm = [4,5,15,16]
         createBlock5(color)
     }else if(form == "+"){
-        positionForm = [4,13,14,15,24]
+        positionForm = [4,13,14,15]
         createBlock5(color)
     }
 }
@@ -150,7 +181,8 @@ const destroyBlocksLine = ()=>{
             if (blocks[0] % 10 === 0) {
                 countBlocks++
                 if (countBlocks == 10) {
-                    console.log(blocks);
+                    addPoints(100)
+                    addLines(1)
                     blocks.forEach(e => {
                         container.children[e].className = "";
                         container.children[e].classList.add("box");  
@@ -172,20 +204,21 @@ const destroyBlocksLine = ()=>{
 
 const lineInterval = setInterval(() => {
 
-    if (currentForm == "square" || currentForm == "z") {
-        if (positionForm[3] >= 190 || positionForm[3] >= 199 || container.children[positionForm[0] + 10].classList.contains(`active`) || container.children[positionForm[3] + 10].classList.contains(`active`)|| container.children[positionForm[2] + 10].classList.contains(`active`) || container.children[positionForm[1] + 10].classList.contains(`active`) || container.children[positionForm[0] + 10].classList.contains(`active`)) {
+    if (currentForm == "square" || currentForm == "z"|| currentForm == "+") {
+        if (positionForm[0] >= 190 ||positionForm[1] >= 190 || positionForm[0] >= 199 || positionForm[3] >= 190 || positionForm[3] >= 199 || container.children[positionForm[0] + 10].classList.contains(`active`) || container.children[positionForm[3] + 10].classList.contains(`active`)|| container.children[positionForm[2] + 10].classList.contains(`active`) || container.children[positionForm[1] + 10].classList.contains(`active`) || container.children[positionForm[0] + 10].classList.contains(`active`)) {
             colision()
+            addPoints(40)
             createBlock(randomColor(colors), randomForm(forms))
             destroyBlocksLine()
         } else {
             moveY()
         }
-    } else if( currentForm == "line"|| currentForm == "l" || currentForm == "+") {
+    } else if( currentForm == "line"|| currentForm == "l" ) {
         if (positionForm[4] >= 190 || positionForm[4] >= 199 || positionForm[0] >= 190 || positionForm[0] >= 199|| container.children[positionForm[4] + 10].classList.contains(`active`) || container.children[positionForm[3] + 10].classList.contains(`active`)|| container.children[positionForm[2] + 10].classList.contains(`active`) || container.children[positionForm[1] + 10].classList.contains(`active`) || container.children[positionForm[0] + 10].classList.contains(`active`)) {
             colision()
+            addPoints(50)
             createBlock(randomColor(colors),randomForm(forms) )
             destroyBlocksLine()
-    
         }else{
             moveY()
         }
@@ -246,6 +279,26 @@ const rotateZV = (x1,x2,x3) => {
     positionForm [1] -= x2
     positionForm [3] -= x3
 }
+const rotateT = (x1,x2,x3) => {
+    positionForm [0] += x1
+    positionForm [1] += x2
+    positionForm [3] -= x3
+}
+const rotateT2 = (x1,x2,x3) => {
+    positionForm [0] += x1
+    positionForm [1] -= x2
+    positionForm [3] += x3
+}
+const rotateT3 = (x1,x2,x3) => {
+    positionForm [0] -= x1
+    positionForm [1] -= x2
+    positionForm [3] += x3
+}
+const rotateT4 = (x1,x2,x3) => {
+    positionForm [0] -= x1
+    positionForm [1] += x2
+    positionForm [3] -= x3
+}
 
 
 const rotate = (x1, x2, y1, y2 )=>{
@@ -287,6 +340,20 @@ const rotate = (x1, x2, y1, y2 )=>{
             currentEjeZ = "vertical"
         }
 
+    }else if(currentForm == "+"){
+        if (currentEjeT == "vertical") {
+            rotateT(x1,x2,y2)
+            currentEjeT = "horizontal"
+        }else if (currentEjeT == "horizontal"){
+            rotateT2(x1,x2,y2)
+            currentEjeT = "vertical1"
+        }else if (currentEjeT =="vertical1"){
+            rotateT3(x1,x2,y2)
+            currentEjeT = "horizontal1"
+        }else if (currentEjeT =="horizontal1"){
+            rotateT4(x1,x2,y2)
+            currentEjeT = "vertical"
+        }
     }else{
         if (currentEjeLine == "horizontal") {
             rotateR(x1, x2, y1, y2)
@@ -308,20 +375,20 @@ const rotate = (x1, x2, y1, y2 )=>{
 
 document.addEventListener("keydown",(e)=>{
 
-    if (currentForm == "square" || currentForm == "z") {
+    if (currentForm == "square" || currentForm == "z"|| currentForm == "+") {
         if (e.key == "ArrowRight") {
-            if (positionForm[3] % 10 === 9 || container.children[positionForm[3] + 1].classList.contains(`active`)|| container.children[positionForm[2] + 1].classList.contains(`active`) || container.children[positionForm[1] + 1].classList.contains(`active`) || container.children[positionForm[0] + 1].classList.contains(`active`)) {  
+            if (positionForm[3] % 10 === 9 || positionForm[2] % 10 === 9 || positionForm[1] % 10 === 9 || positionForm[0] % 10 === 9 || container.children[positionForm[3] + 1].classList.contains(`active`)|| container.children[positionForm[2] + 1].classList.contains(`active`) || container.children[positionForm[1] + 1].classList.contains(`active`) || container.children[positionForm[0] + 1].classList.contains(`active`)) {  
             }else{
                 rightMove()
             }
         
         }else if(e.key == "ArrowLeft"){
-            if (positionForm[0] % 10 === 0|| container.children[positionForm[3] - 1].classList.contains(`active`)|| container.children[positionForm[2] - 1].classList.contains(`active`) || container.children[positionForm[1] - 1].classList.contains(`active`) || container.children[positionForm[0] - 1].classList.contains(`active`)) {            
+            if (positionForm[0] % 10 === 0 || positionForm[1] % 10 === 0 || positionForm[2] % 10 === 0 || positionForm[3] % 10 === 0 || container.children[positionForm[3] - 1].classList.contains(`active`)|| container.children[positionForm[2] - 1].classList.contains(`active`) || container.children[positionForm[1] - 1].classList.contains(`active`) || container.children[positionForm[0] - 1].classList.contains(`active`)) {            
             }else {        
                 leftMove()
             }
         }else if(e.key == "ArrowDown"){
-            if (positionForm[4] >= 180 || positionForm[3] >= 189 || container.children[positionForm[3] + 10].classList.contains(`active`) || container.children[positionForm[2] + 10].classList.contains(`active`) || container.children[positionForm[1] + 10].classList.contains(`active`) || container.children[positionForm[0] + 10].classList.contains(`active`)){
+            if (positionForm[0] >= 180 || positionForm[1] >= 189|| positionForm[1] >= 189 ||positionForm[4] >= 180 || positionForm[3] >= 189 || container.children[positionForm[3] + 10].classList.contains(`active`) || container.children[positionForm[2] + 10].classList.contains(`active`) || container.children[positionForm[1] + 10].classList.contains(`active`) || container.children[positionForm[0] + 10].classList.contains(`active`)){
             }else{
                 downMove()
             }
@@ -337,10 +404,20 @@ document.addEventListener("keydown",(e)=>{
             }else if (currentEjeZ == "horizontal1" && positionForm[2] % 10 !== 9 && positionForm[2] % 10 !== 0&& positionForm[0] % 10 !== 9 && !container.children[positionForm[0] - 1].classList.contains("active") && !container.children[positionForm[1] - 11].classList.contains("active") && !container.children[positionForm[3] - 10].classList.contains("active")){
                 rotate(1,11,0,10)
             }
+        }else if(currentForm == "+" ){
+            if (currentEjeT == "vertical") {
+                rotate(9,11,0,11) 
+            } else if(currentEjeT == "horizontal"){
+                rotate(11,9,0,9) 
+            }else if(currentEjeT == "vertical1"){
+                rotate(9,11,0,11) 
+            }else if(currentEjeT == "horizontal1"){
+                rotate(11,9,0,9) 
+            }
         }
 }        
 
-    } else if(currentForm == "line" || currentForm == "l" || currentForm == "+"){
+    } else if(currentForm == "line" || currentForm == "l" ){
         if (e.key == "ArrowRight") {
             if (positionForm[3] % 10 === 9 ||positionForm[0] % 10 === 9 || positionForm[4] % 10 === 9 ||container.children[positionForm[4] + 1].classList.contains(`active`) || container.children[positionForm[3] + 1].classList.contains(`active`)|| container.children[positionForm[2] + 1].classList.contains(`active`) || container.children[positionForm[1] + 1].classList.contains(`active`) || container.children[positionForm[0] + 1].classList.contains(`active`)) {  
             }else{
@@ -355,7 +432,7 @@ document.addEventListener("keydown",(e)=>{
             }
     
         }else if(e.key == "ArrowDown"){
-            if (positionForm[4] >= 180 || positionForm[4] >= 189||container.children[positionForm[4] + 10].classList.contains(`active`) ||container.children[positionForm[3] + 10].classList.contains(`active`) || container.children[positionForm[2] + 10].classList.contains(`active`) ||container.children[positionForm[1] + 10].classList.contains(`active`) ||container.children[positionForm[0] + 10].classList.contains(`active`)) {
+            if (positionForm[0] >= 180 || positionForm[0] >= 189||positionForm[4] >= 180 || positionForm[4] >= 189||container.children[positionForm[4] + 10].classList.contains(`active`) ||container.children[positionForm[3] + 10].classList.contains(`active`) || container.children[positionForm[2] + 10].classList.contains(`active`) ||container.children[positionForm[1] + 10].classList.contains(`active`) ||container.children[positionForm[0] + 10].classList.contains(`active`)) {
             }else{
                 downMove()
             }
@@ -384,3 +461,4 @@ document.addEventListener("keydown",(e)=>{
 
 
 })
+
